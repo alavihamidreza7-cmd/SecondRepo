67bcdf58fdece5643561e3849426b29b1a1219dd
alavihamidreza7@gmail.com <HAMIDMILAD@#386>
2026-07-17 10:49:08 +0330

    Frequently Cpoy_Paste

--- ContactingFiles.cpp
+++ ContactingFiles.cpp
@@ -1,3 +1,28 @@
+
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
 #include<stdio.h>
 #include <stdlib.h>
 #include<string.h>
--- Reviewing.c
+++ Reviewing.c
@@ -1,4 +1,31 @@
+
 #i#include<stdio.h>
 #include <stdlib.h>
 #include<string.h>
+#i#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#i#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#i#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+#i#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#i#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#i#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#i#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#i#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
 
