47e3db8dbaccec5911436de19f61afbd5596e610
alavihamidreza7@gmail.com <HAMIDMILAD@#386>
2026-07-17 13:17:41 +0330

    Deleting

--- TreeException.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-#include"TreeException.h"
-using namespace std;
-TreeException::TreeException():length_error("CAUTION :: This Tree Is Empty Then You Can Not Eliminate It !!") {
-
-}
